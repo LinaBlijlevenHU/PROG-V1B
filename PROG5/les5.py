af5b21969dcca3656b1d5767fb2440daa00dfc38
Lina Blijleven <selinablijleven@gmail.com>
2022-09-21 11:17:05 +0200

    Les 5 voorbeelden

---
+++ PROG5/les5.py
@@ -0,0 +1,25 @@
+def pythagoras(a, b):
+    'Bereken de lengte van een lange zijde met de stelling van Pythagoras
+    @param  int|float   Eerste korte zijde
+    @param  int|float   Tweede korte zijde
+    @return float       Lengte van de lange zijde
+    '
+    # Bereken a^2 + b^2
+    ab = a**2 + b**2
+    # Neem daar de wortel van
+    c = ab**(1/2)
+    return c
+
+# Bereken de lengte van de lange zijde
+print(pythagoras(3, 4))
+
+# Wat staat er nu in c?
+# Error: de waarde blijft binnen de functie
+#print(c)
+
+# Print getallen in een range
+for i in range(5):
+    print(i)
+
+# Wat staat er nu in i?
+print(i)
