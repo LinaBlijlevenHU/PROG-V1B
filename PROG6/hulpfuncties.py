0aea514826acd04aa60acad576b0598eef5f64bb
Lina Blijleven <selinablijleven@gmail.com>
2022-09-26 10:55:20 +0200

    Les 6 voorbeelden (excl comments)

---
+++ PROG6/hulpfuncties.py
@@ -0,0 +1,10 @@
+def opsommen(lst):
+    return sum(lst)
+
+def opsommen2(lst):
+    som = 0
+
+    for e in lst:
+        som += e
+
+    return som
---
+++ PROG6/les6.py
@@ -0,0 +1,12 @@
+import hulpfuncties
+from hulpfuncties import opsommen2
+
+a = "Hallo, WelkOm bij de les"
+
+print(a.lower())
+
+som = hulpfuncties.opsommen([1, 2, 3, 4, 5, 6])
+print(som)
+
+som2 = opsommen2([1, 2, 3, 5, 4, 3])
+print(som2)
