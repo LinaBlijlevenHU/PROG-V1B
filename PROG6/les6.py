7d9fa5024b2da6b051a5248d865f1b0f777aeb6c
Lina Blijleven <selinablijleven@gmail.com>
2022-09-26 11:41:07 +0200

    Comments voor les 6

--- PROG6/les6.py
+++ PROG6/les6.py
@@ -1,12 +1,24 @@
+# Importeer het hele bestand hulpfuncties
+# Je kunt nu methodes hieruit gebruiken met hulpfuncties.functienaam()
+# Let op: dit kan langzaam zijn. Als we maar één methode nodig hebben kunnen
+# we beter de aanpak hieronder gebruiken.
 import hulpfuncties
+
+# Importeer een enkele methode uit het bestand hulpfuncties.
+# Je kunt deze nu gebruiken alsof hij direct in het bestand staat.
 from hulpfuncties import opsommen2
 
+# Maak een string aan met wat willekeurige hoofdletters.
 a = "Hallo, WelkOm bij de les"
-
+# Print deze met alleen kleine letters. (We hebben ook .upper() en .capitalize())
 print(a.lower())
 
+# Gebruik een functie uit een bestand dat we geimporteerd hebben.
+# We specificeren hier ook het bestand/de module.
 som = hulpfuncties.opsommen([1, 2, 3, 4, 5, 6])
 print(som)
 
+# Gebruik een geimporteerde functie direct.
+# Let op: hier staat dus geen hulpfuncties met een punt voor.
 som2 = opsommen2([1, 2, 3, 5, 4, 3])
 print(som2)
